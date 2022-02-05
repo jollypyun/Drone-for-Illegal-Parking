f8315423f6c7947d7f3a2b40238ae7577fa22298
jollypyun <58629366+jollypyun@users.noreply.github.com>
2022-02-06 03:09:12 +0900

    - Contour 처리까지

--- main.py
+++ main.py
@@ -25,15 +25,16 @@ while cv2.waitKey(33) < 0:
     imgThresh = cv2.adaptiveThreshold(imgBlur, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY_INV, blockSize=19, C=9) # Threshold 처리
 
     contours, _ = cv2.findContours(imgThresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE) # Contour 처리
-    temp_result = np.zeros((800,800, 1), dtype=np.uint8)
+    temp_result = np.zeros((800, 800, 1), dtype=np.uint8)
     cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
-    temp_result = np.zeros((800,800, 1), dtype=np.uint8)
+    temp_result = np.zeros((800, 800, 1), dtype=np.uint8)
     contours_dict = []
 
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(temp_result, pt1=(x,y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=2)
+        cv2.rectangle(temp_result, pt1=(x, y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=2)
+        contours_dict.append({'contour': contour, 'x': x, 'y': y, 'w': w, 'h': h, 'cx': x + (w/2), 'cy': y + (h/2)})
 
-    cv2.imshow("VideoFrame", imgThresh)
+    cv2.imshow("VideoFrame", temp_result)
 capture.release()
 cv2.destroyAllWindows()
